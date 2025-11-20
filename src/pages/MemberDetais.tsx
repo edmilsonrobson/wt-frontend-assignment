a12e817d4442b3f1fd5da1da95c4be04dd4c9cf6
Ed <edmilsonrobson@gmail.com>
2025-11-20 15:12:59 -0300

    add delete member functionality on MemberDetails

--- src/pages/MemberDetais.tsx
+++ src/pages/MemberDetais.tsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import styled from "styled-components";
 
-import { getSingleMember } from "../api/members";
-import { Container, Typography } from "@mui/material";
+import { deleteMember, getSingleMember } from "../api/members";
+import {
+  Container,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import { UserAvatar } from "../components/UserAvatar";
+import { useState } from "react";
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -17,6 +26,17 @@ const StyledContainer = styled(Container)`
 
 export const MemberDetails = () => {
   const { id = "" } = useParams<{ id: string }>();
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: deleteMember,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["members"] });
+      navigate("/");
+    },
+  });
   const {
     data: member,
     isLoading,
@@ -46,6 +66,19 @@ export const MemberDetails = () => {
     );
   }
 
+  const handleDeleteButtonClick = () => {
+    setIsDeleteDialogOpen(true);
+  };
+
+  const handleDeleteDialogClose = () => {
+    setIsDeleteDialogOpen(false);
+  };
+
+  const handleDeleteConfirm = () => {
+    mutation.mutate(id);
+    navigate("/");
+  };
+
   return (
     <StyledContainer maxWidth="md">
       <Typography variant="h2" component="h1" gutterBottom>
@@ -53,6 +86,37 @@ export const MemberDetails = () => {
       </Typography>
       <UserAvatar member={member} />
       <Typography>Member Details {id}</Typography>
+
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleDeleteButtonClick}
+      >
+        Delete
+      </Button>
+      <Dialog open={isDeleteDialogOpen} onClose={handleDeleteDialogClose}>
+        <DialogTitle color="error">Delete Member - Danger Zone</DialogTitle>
+        <DialogContent>
+          <Typography>Are you sure you want to delete this member?</Typography>
+        </DialogContent>
+
+        <DialogActions>
+          <Button
+            variant="text"
+            color="secondary"
+            onClick={handleDeleteDialogClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={handleDeleteConfirm}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </StyledContainer>
   );
 };
